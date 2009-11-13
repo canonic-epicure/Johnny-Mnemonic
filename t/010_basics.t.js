4902ba0f5110c78eecd4397120131df2849b2a7d
SamuraiJack <nickolay8@gmail.com>
2009-11-13 18:05:30 +0300

    - tested basic scenario, passing FF & IE7

--- t/010_basics.t.js
+++ t/010_basics.t.js
@@ -2,15 +2,77 @@ StartTest(function(t) {
 	
     t.plan(1)
     
+    //==================================================================================================================================================================================
+    t.diag("Starting test")
+    
+    
     var async1 = t.beginAsync()
     
     
-    onStateChange = function (token) {
-        t.pass('onstate reached')
-        
-        t.endAsync(async1)
-    }
+    var firstRun = true
     
+    run = function (mnemonic, window) {
+        if (firstRun) {
+            firstRun = false
+            
+            var location = window.location
+            var history  = window.history
+            
+            t.pass("'setup' fired a callback")
+
+            //==================================================================================================================================================================================
+            t.diag("Remembering")
+            
+            mnemonic.remember(10)
+            t.ok(location.hash == '#10', "Hash was correctly set after 'remember'")
+            
+            mnemonic.remember(20)
+            t.ok(location.hash == '#20', "Hash was correctly set after 'remember'")
+            
+            mnemonic.remember(30)
+            t.ok(location.hash == '#30', "Hash was correctly set after 'remember'")
+
+            
+            //==================================================================================================================================================================================
+            t.diag("Recalling")
+            
+            history.back()
+            
+            mnemonic.on('statechange', function (mnemonic, token) {
+                t.ok(token == '20', 'Correctly recalled previous state')
+                t.ok(location.hash == '#20', " .. indeed")
+                
+                history.back()
+                
+                mnemonic.on('statechange', function (mnemonic, token) {
+                    t.ok(token == '10', 'Correctly recalled previous state')
+                    t.ok(location.hash == '#10', " .. indeed")
+
+                    
+                    history.forward()
+                    
+                    mnemonic.on('statechange', function (mnemonic, token) {
+                        t.ok(token == '20', 'Correctly recalled next state')
+                        t.ok(location.hash == '#20', " .. indeed")
+
+                        
+                        history.forward()
+                        
+                        mnemonic.on('statechange', function (mnemonic, token) {
+                            t.ok(token == '30', 'Correctly recalled next state')
+                            t.ok(location.hash == '#30', " .. indeed")
+                            
+                            t.endAsync(async1)
+                            
+                        }, this, { single : true })
+                        
+                    }, this, { single : true })
+                    
+                }, this, { single : true })
+                
+            }, this, { single : true })
+        }
+    }
     
     
     
