e9a257711b6fda160bc5d3dfb2d03982fbd3082a
SamuraiJack <nickolay8@gmail.com>
2009-11-13 18:15:03 +0300

    - switched to non-system identifiers

--- t/010_basics.t.js
+++ t/010_basics.t.js
@@ -1,6 +1,6 @@
 StartTest(function(t) {
 	
-    t.plan(1)
+    t.plan(12)
     
     //==================================================================================================================================================================================
     t.diag("Starting test")
@@ -11,12 +11,12 @@ StartTest(function(t) {
     
     var firstRun = true
     
-    run = function (mnemonic, window) {
+    run = function (mnemonic, testWindow) {
         if (firstRun) {
             firstRun = false
             
-            var location = window.location
-            var history  = window.history
+            var testLocation = testWindow.location
+            var testHistory  = testWindow.history
             
             t.pass("'setup' fired a callback")
 
@@ -24,43 +24,43 @@ StartTest(function(t) {
             t.diag("Remembering")
             
             mnemonic.remember(10)
-            t.ok(location.hash == '#10', "Hash was correctly set after 'remember'")
+            t.ok(testLocation.hash == '#10', "Hash was correctly set after 'remember'")
             
             mnemonic.remember(20)
-            t.ok(location.hash == '#20', "Hash was correctly set after 'remember'")
+            t.ok(testLocation.hash == '#20', "Hash was correctly set after 'remember'")
             
             mnemonic.remember(30)
-            t.ok(location.hash == '#30', "Hash was correctly set after 'remember'")
+            t.ok(testLocation.hash == '#30', "Hash was correctly set after 'remember'")
 
             
             //==================================================================================================================================================================================
             t.diag("Recalling")
             
-            history.back()
+            testHistory.back()
             
             mnemonic.on('statechange', function (mnemonic, token) {
                 t.ok(token == '20', 'Correctly recalled previous state')
-                t.ok(location.hash == '#20', " .. indeed")
+                t.ok(testLocation.hash == '#20', " .. indeed")
                 
-                history.back()
+                testHistory.back()
                 
                 mnemonic.on('statechange', function (mnemonic, token) {
                     t.ok(token == '10', 'Correctly recalled previous state')
-                    t.ok(location.hash == '#10', " .. indeed")
+                    t.ok(testLocation.hash == '#10', " .. indeed")
 
                     
-                    history.forward()
+                    testHistory.forward()
                     
                     mnemonic.on('statechange', function (mnemonic, token) {
                         t.ok(token == '20', 'Correctly recalled next state')
-                        t.ok(location.hash == '#20', " .. indeed")
+                        t.ok(testLocation.hash == '#20', " .. indeed")
 
                         
-                        history.forward()
+                        testHistory.forward()
                         
                         mnemonic.on('statechange', function (mnemonic, token) {
                             t.ok(token == '30', 'Correctly recalled next state')
-                            t.ok(location.hash == '#30', " .. indeed")
+                            t.ok(testLocation.hash == '#30', " .. indeed")
                             
                             t.endAsync(async1)
                             
@@ -73,38 +73,5 @@ StartTest(function(t) {
             }, this, { single : true })
         }
     }
-    
-    
-    
-//    var async1 = t.beginAsync()
-//    
-//    
-//    use('Johnny.Mnemonic', function () {
-//        
-//        //==================================================================================================================================================================================
-//        t.diag("Sanity")
-//        
-//        t.ok(Johnny.Mnemonic, "Johnny.Mnemonic is here")
-//        
-//        
-//        //==================================================================================================================================================================================
-//        t.diag("Instantiation")
-//        
-//        var mnemonic = new Johnny.Mnemonic({
-//            hashFrom : 'window',
-//            
-//            defaultToken : '1'
-//        })
-//        
-//        t.ok(mnemonic, "'Johnny.Mnemonic' was successfully instantiated")
-//        
-//        
-//        //==================================================================================================================================================================================
-//        t.diag("Setup")
-//        
-//        mnemonic.setup()
-//        
-//        t.endAsync(async1)
-//    })
     
 })
